6fc1e06f8daf2f27d380d48bbe38951b2f203976
Camilo <alvarezdc777@gmail.com>
2019-02-21 21:45:16 -0500

    documentacion de inventario y venta

--- proyectoLibreria/src/main/java/com/mycompany/proyectolibreria/Inventario.java
+++ proyectoLibreria/src/main/java/com/mycompany/proyectolibreria/Inventario.java
@@ -14,35 +14,96 @@ import java.util.List;
  */
 public class Inventario{
     /**
-     *arreglo en el que contiene todos los libros
+     * variable que identifica el inventario
+     */
+    private int id_inventario;
+    /**
+     *variable, llave foranea que relaciona la clase libro con inventario
+     */
+    private int id_libro;
+    /**
+     *arreglo que contiene todos los libros
      */
     private Libro[] arregloLibros;
 
+    /**
+     * constructor que crea un objero del arreglo
+     */
     public Inventario() {
         arregloLibros = new Libro[10];
     }
-   
+   /**
+    * @author CamiloAlvarez
+    * funcion que muestra el arreglo de libros
+    * @version 1.0
+    */
     public void mostrarLibros(){
         
     }
-    /*@author CamiloAlvarez*/
+    /**
+     * @author CamiloAlvarez
+     * funcion que clasifica por genero los libros
+     * @version 1.0
+     */
     public void clasificarLibros(){
         
     }
-    /*@author CamiloAlvarez*/
+    /**
+     * @author CamiloAlvarez
+     * funcion que busca un libro en especifico
+     * @version 1.0
+     */
     public void buscarLibro(){
         
     }
-    /*@author CamiloAlvarez*/
+    /**
+     * @author CamiloAlvarez
+     * funcion que selecciona el libro mas caro
+     * @version 1.0
+     */
     public void libroMasCaro(){
         
     }
-    /*@author CamiloAlvarez*/
+     /**
+     * @author CamiloAlvarez
+     * funcion que selecciona el libro menos caro
+     * @version 1.0
+     */
     public void libroMenosCaro(){
         
     } 
-
+ /**
+     * @author CamiloAlvarez
+     * funcion que valida la cantidad minima de cada libro 
+     * @version 1.0
+     */
     public void libroCantidadMinimo() {
     }
+
+    public int getId_inventario() {
+        return id_inventario;
+    }
+
+    public void setId_inventario(int id_inventario) {
+        this.id_inventario = id_inventario;
+    }
+
+    public int getId_libro() {
+        return id_libro;
+    }
+
+    public void setId_libro(int id_libro) {
+        this.id_libro = id_libro;
+    }
+
+    public Libro[] getArregloLibros() {
+        return arregloLibros;
+    }
+
+    public void setArregloLibros(Libro[] arregloLibros) {
+        this.arregloLibros = arregloLibros;
+    }
+    
+    
     
 }
--- proyectoLibreria/src/main/java/com/mycompany/proyectolibreria/Venta.java
+++ proyectoLibreria/src/main/java/com/mycompany/proyectolibreria/Venta.java
@@ -8,19 +8,93 @@ package com.mycompany.proyectolibreria;
 import java.sql.Time;
 
 /**
- *
  * @author CamiloAlvarez
+ * clase Venta creada para registrar las ventas de cada cliente
+ * @version 1.0
  */
 public class Venta{
-    
-    private String formasDePago;
+    /**
+     * variable, llave primaria que identifica cada venta
+     */
+    private int id_venta;
+    /**
+     * variable,llave foranea que relaciona la clase Carrito con Inventario
+     */
+    private int id_carrito;
+    /**
+     * variable, llave foranea que relaciona la clase Cliente con Venta
+     */
+    private int id_cedula;
+    /**
+     * arreglo que contiene las formas de pago
+     */
+    private Venta[] formasDePago;
+    /**
+     * variable que contiene el estado de la venta
+     */
     private int estado;
+/**
+ * Constructor de recibe todos los parametros  para ejecutar.
+ * @param id_venta llave primaria para identifiar cada venta
+ * @param id_carrito llave foranea para relacionar clases
+ * @param id_cedula llave foranea para relacionar clases
+ * @param formasDePago arreglo que contiene las formas de pago
+ * @param estado  variable que contiene estado de la venta
+ */
+    public Venta(int id_venta, int id_carrito, int id_cedula, Venta[] formasDePago, int estado) {
+        this.id_venta = id_venta;
+        this.id_carrito = id_carrito;
+        this.id_cedula = id_cedula;
+        this.formasDePago = formasDePago;
+        this.estado = estado;
+    }
+   /**
+ * @author CamiloAlvarez
+ * funcion que seleciona el libro mas vendido
+ * @version 1.0
+ */
+    public void libroMasVendido(){
+        
+    }
+      /**
+ * @author CamiloAlvarez
+ * funcion que seleciona el libro menos vendido
+ * @version 1.0
+ */
+    
+    public void libroMenosVendido(){
+        
+    }
 
-    public String getFormasDePago() {
+    public int getId_venta() {
+        return id_venta;
+    }
+
+    public void setId_venta(int id_venta) {
+        this.id_venta = id_venta;
+    }
+
+    public int getId_carrito() {
+        return id_carrito;
+    }
+
+    public void setId_carrito(int id_carrito) {
+        this.id_carrito = id_carrito;
+    }
+
+    public int getId_cedula() {
+        return id_cedula;
+    }
+
+    public void setId_cedula(int id_cedula) {
+        this.id_cedula = id_cedula;
+    }
+
+    public Venta[] getFormasDePago() {
         return formasDePago;
     }
 
-    public void setFormasDePago(String formasDePago) {
+    public void setFormasDePago(Venta[] formasDePago) {
         this.formasDePago = formasDePago;
     }
 
@@ -30,15 +104,6 @@ public class Venta{
 
     public void setEstado(int estado) {
         this.estado = estado;
-    }
- 
-    /*@author CamiloAlvarez*/
-    public void libroMasVendido(){
-        
-    }
-    /*@author CamiloAlvarez*/
-    public void libroMenosVendido(){
-        
     }
     
     
